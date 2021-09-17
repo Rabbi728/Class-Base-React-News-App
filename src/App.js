0a31443f75c48f21deb67318942d984fce1156e6
Rabbi Ahamed <rabbiahamed728@gmail.com>
2021-09-17 20:09:41 +0600

    add top loading bar and env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './components/layouts/Navbar'
 import List from './components/pages/List'
+import LoadingBar from 'react-top-loading-bar'
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,19 +9,34 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  state = {
+    progress     : 0,
+  }
+
+  setProgress = (progress)=>{
+    this.setState({
+      progress      : progress,
+    })
+  }
+
   render() {
     return (
       <>
         <Router>
           <Navbar />
+          <LoadingBar
+            color="#f11946"
+            height={3}
+            progress={this.state.progress}
+          />
           <Switch>
-            <Route exact path="/business"> <List key="business" category="business" /> </Route>
-            <Route exact path="/entertainment"> <List key="entertainment" category="entertainment" /> </Route>
-            <Route exact path="/"> <List key="general" category="general" /> </Route>
-            <Route exact path="/health"> <List key="health" category="health" /> </Route>
-            <Route exact path="/science"> <List key="science" category="science" /> </Route>
-            <Route exact path="/sports"> <List key="sports" category="sports" /> </Route>
-            <Route exact path="/technology"> <List key="technology" category="technology" /> </Route>
+            <Route exact path="/business"> <List setProgress={this.setProgress} key="business" category="business" /> </Route>
+            <Route exact path="/entertainment"> <List setProgress={this.setProgress} key="entertainment" category="entertainment" /> </Route>
+            <Route exact path="/"> <List setProgress={this.setProgress} key="general" category="general" /> </Route>
+            <Route exact path="/health"> <List setProgress={this.setProgress} key="health" category="health" /> </Route>
+            <Route exact path="/science"> <List setProgress={this.setProgress} key="science" category="science" /> </Route>
+            <Route exact path="/sports"> <List setProgress={this.setProgress} key="sports" category="sports" /> </Route>
+            <Route exact path="/technology"> <List setProgress={this.setProgress} key="technology" category="technology" /> </Route>
           </Switch>
         </Router>
       </>
--- src/components/pages/List.js
+++ src/components/pages/List.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import Loader from '../layouts/Loader';
-import Pagination from '../layouts/Pagination';
 import Info from './Info'
+import InfiniteScroll from 'react-infinite-scroll-component';
+// import Pagination from '../layouts/Pagination';
 
 export class List extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { 
-            article     : [],
-            pageItem    : 6,
-            page        : 1,
+        this.state = {
+            article: [],
+            pageItem: 6,
+            page: 1,
             totalResults: 0,
-            loading     : false
+            loading: false,
+            hasMore: false,
         }
-        document.title = this.wordCapitalize(props.category)+ " - News App"
+        document.title = this.wordCapitalize(props.category) + " - News App"
     }
 
     wordCapitalize = (word) => {
@@ -22,21 +24,36 @@ export class List extends Component {
     }
 
     fetchArticleDatas = async () => {
-        this.setState({loading : true})
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=f283e6a592664325bd97a57fdffec091&pageSize=${this.state.pageItem}&page=${this.state.page}`;
+        this.props.setProgress(10)
+        this.setState({ loading: true })
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${this.state.pageItem}&page=${this.state.page}`;
+        let res = await fetch(url);
+        this.props.setProgress(30)
+        let data = await res.json();
+        this.props.setProgress(70) 
+        this.setState({
+            article: data.articles,
+            totalResults: data.totalResults,
+            loading: false
+        })
+        this.props.setProgress(100)
+    }
+
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 })
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${this.state.pageItem}&page=${this.state.page}`;
         await fetch(url)
             .then((res) => res.json())
-            .then((data) => 
-                this.setState((state, props)=>({
-                    article     : data.articles,
+            .then((data) =>
+                this.setState((state, props) => ({
+                    article: state.article.concat(data.articles),
                     totalResults: data.totalResults,
-                    loading     : false
                 }))
             );
     }
 
     loadAfterClickPagination = (pageNumber) => {
-        this.setState({page : pageNumber})
+        this.setState({ page: pageNumber })
         this.fetchArticleDatas();
     }
 
@@ -46,18 +63,28 @@ export class List extends Component {
 
     render() {
         return (
-            <div className="container my-3">
-                <h1 className="my-3">News Headline --</h1>
-                {this.state.loading && <Loader/>}
-                {!this.state.loading && this.state.article && <div className="row mb-5">
-                    {this.state.article.map((el) =>{
-                        return  <div key={el.url} className="col-md-4">
+            <>
+                <h2 className="my-4 text-center">Top Headline From {this.wordCapitalize(this.props.category)}</h2>
+                {this.state.loading && <Loader />}
+                <InfiniteScroll
+                    dataLength={this.state.article.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.article.length !== this.state.totalResults}
+                    loader={<Loader />}
+                >
+                    <div className="container">
+                        <div className="row mb-5">
+
+                            {this.state.article.map((el) => {
+                                return <div key={el.url} className="col-md-4">
                                     <Info details={el} />
                                 </div>
-                    })}
-                </div>}
-                {!this.state.loading && this.state.totalResults > 0 && <Pagination loadData={this.loadAfterClickPagination} page={this.state.page} pageItem={this.state.pageItem} totalResults={this.state.totalResults}/>}
-            </div>
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+                {/* {!this.state.loading && this.state.totalResults > 0 && <Pagination loadData={this.loadAfterClickPagination} page={this.state.page} pageItem={this.state.pageItem} totalResults={this.state.totalResults}/>} */}
+            </>
         )
     }
 }
